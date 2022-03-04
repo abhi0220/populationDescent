357865ad8a1ffb47c3b988f13bb212a51a50cd07
Bassel Mabsout <bmabsout@hotmail.com>
2022-03-04 11:29:16 -0500

    added nix based envs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "nixpkgs/02336c5c5f719cd6bd4cfc5a091a1ccee6f06b1d";
+    nixpkgs.url = "nixpkgs/nixos-21.11";
     mach-nix.url = github:DavHau/mach-nix;
   };
 
@@ -13,17 +13,11 @@
       let pkgs = (import (inputs.nixpkgs) { config = {allowUnfree = true;}; system =
               "x86_64-linux";
                   });
-                  
-
-          mach-nix-utils = import inputs.mach-nix {
-            inherit pkgs;
-            python = "python39Full";
-            #pypiDataRev = "e18f4c312ce4bcdd20a7b9e861b3c5eb7cac22c4";
-            #pypiDataSha256= "sha256-DmrRc4Y0GbxlORsmIDhj8gtmW1iO8/44bShAyvz6bHk=";
-          };
-
           
-          python-with-deps = mach-nix-utils.mkPython {
+          python-with-deps = inputs.mach-nix.lib."${system}".mkPython {
+            providers = {
+              tensorflwo="nixpkgs";
+            };
 
             requirements=''
               numpy
